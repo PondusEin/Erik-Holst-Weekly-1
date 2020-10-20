7c3b28a2d3800f90ff2f14a9b2d2e9f918dab263
PondusEin <PondusEin@users.noreply.github.com>
2020-10-20 10:57:28 +0200

    Added Header files to each task. Everything works better now

--- Erik Holst Weekly 1/Erik Holst Weekly 1.cpp
+++ Erik Holst Weekly 1/Erik Holst Weekly 1.cpp
@@ -5,6 +5,11 @@
 #include <conio.h>
 #include <limits>
 #include <windows.h>
+#include "Task_1.h"
+#include "Task_2.h"
+#include "Task_3.h"
+#include "Task_4.h"
+#include "Task_5.h"
 
 int getintfromuser() {
 	int integer;
@@ -21,14 +26,6 @@ int getintfromuser() {
 
 void task1()
 {
-	int age;
-	char initial;
-	std::string Fullname;
-	long tlf;
-	int BirthDay;
-	int BirthMonth;
-	int BirthYear;
-
 	//input
 	system("cls");
 	std::cout << "Hello there! May I inquire your full name?\n";
@@ -36,22 +33,17 @@ void task1()
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, Fullname);
 	initial = Fullname[0];
-
 	std::cout << "Nice meeting you, " << Fullname << "! \n\nHow old are you?\n";
 	std::cout << "Age: ";
 	std::cin >> age;
-
 	while (std::cin.fail()) {
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
 		std::cout << "That, " << Fullname << ", is NOT a number..\n\n";
 		std::cout << "How old are you? ";
 		std::cin >> age;
-		
 	}
 	std::cout << "Wow! You're " << age << " years old?\n";
-		
 		if (age >= 60) {
 			std::cout << "You ARE really old!";
 		}
@@ -67,27 +59,19 @@ void task1()
 		else {
 			std::cout << "You probably shouldn't be here. \nFuck off.";
 		}
-
 	std::cout << "\n\nWhat's your phone number " << Fullname <<"? Don't worry, I won't share it!\n";
 	std::cout << "Phone number: ";
 	std::cin >> tlf;
-
 	std::cout << "\nCool. Cool cool cool. \n\nI definitely won't share your phone number " << tlf << " with the chinese government...\n\n";
-	
-	
-		
 	std::cout << "Say, when is your birthday, " << Fullname << "? \nNot that I need it for anything non-suspicious...\n";
 	std::cout << "Year: ";
 	std::cin >> BirthYear;
-
 	std::cout << "Cool, what month are you born?\n";
 	std::cout << "Month: ";
 	std::cin >> BirthMonth;
-
 	std::cout << "Wow, how interesting. What day?\n";
 	std::cout << "Day: ";
 	std::cin >> BirthDay;
-
 	//output for task one
 	system("cls");
 	std::cout << "----------------------------------------------------\n";
@@ -101,33 +85,22 @@ void task1()
 	std::cout << "----------------------------------------------------\n";
 	std::cout << "| Birthday:\t|" << BirthDay << "/" << BirthMonth << "/" << BirthYear << "\t"<< std::endl;
 	std::cout << "----------------------------------------------------\n";
-
 	std::cout << "\n\n\nPress any key to continue to another task, " << Fullname << "!";
 	char heyo = _getch();
 }
 
 void task2() {
-	std::string name;
-	int age2;
-	std::string answertext;
 	char YN = '0';
-
 	system("cls");
 	std::cout << "What is your name again?\n" << std::endl;
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, name); 
-
 	system("cls");
 	std::cout << "Right, right... and how old are you again?\n";
-	std::cin >> age2; //A bug seems to happen here when you've done task one. Giving you the age of 0 automatically.
-	
+	std::cin >> age2; //A bug seems to happen here when you've done task one. Giving you the age of 0 automatically.	
 	system("cls");
 	std::cout << "Cool Cool... Do you drink coffee?\n y/n: ";
-
 	std::cin >> YN;
-	
-
-	
 	if (YN == 'y') {
 		answertext = "Seems you like coffee";
 	}
@@ -137,32 +110,21 @@ void task2() {
 	else {
 		answertext = "You'll have water then!";
 	}
-
-
-
 	system("cls");
 	std::cout << "Your name is: " << name << std::endl;
-
 	std::cout << "You are " << age2 << " years old!" << std::endl;
-
 	std::cout << answertext << std::endl;
-
 	std::cout << "\n\n\nPress any key to continue to another task, " << name << "!";
 	char heyo2 = _getch();
-
 }
 
 void task3() {
-	int age;
-	
 	system("cls");
 	std::cout << "How old are you? \n";
 	std::cout << "Age: ";
 	std::cin >> age;
-
 	//Output
 	std::cout << "Wow! You're " << age << " years old?\n";
-
 	if (age >= 60) {
 		std::cout << "You ARE really old!";
 	}
@@ -185,7 +147,6 @@ void task3() {
 
 void task4() {
 	//input
-	int drink;
 	system("cls");
 	std::cout << "Hello again, what is your favourite drink? \n1. Coffee\n2. Tea\n3. Coca Cola" << std::endl;
 	std::cin >> drink;
@@ -202,14 +163,11 @@ void task4() {
 		system("cls");
 		std::cout << "That's not on the list, so I'll get you some water!\n\n";
 	}
-
 	std::cout << "\n\n\nPress any key to continue to another task!";
 	char heyo = _getch();
 }
 
 void task5() {
-	std::string design;
-	
 	system("cls");
 	design += "      1   2   3   4   5   6    \n";
 	design += "     --- --- --- --- --- ---   \n";
@@ -222,7 +180,6 @@ void task5() {
 	design += " D  | * | * | * | * | * | * | D\n";
 	design += "     --- --- --- --- --- ---   \n";
 	design += "      1   2   3   4   5   6    \n";
-
 	for (int i = 0; i < design.length(); i++)  //Warning: C4018
 	{
 		if (design.at(i) == '*')
@@ -236,8 +193,6 @@ void task5() {
 			std::cout << design.at(i);
 		}
 	}
-
-
 	std::cout << "\n\n\nPress any key to continue to another task!";
 	char heyo = _getch();
 }
@@ -248,8 +203,6 @@ int main() {
 	bool complete3 = false;
 	bool complete4 = false;
 	bool complete5 = false;
-
-
 	start:
 	system("cls");
 	std::cout << "This is Erik Holst's assignment Weekly 1.\n";
@@ -264,7 +217,6 @@ choice:
 	}
 	else {
 		int choice = getintfromuser();
-
 		if (choice == 1) {
 			task1();
 			complete1 = true;
@@ -293,5 +245,4 @@ choice:
 		}
 		goto start;
 	}
-	
 }
---
+++ Erik Holst Weekly 1/Task_1.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+int age;
+char initial;
+std::string Fullname;
+long tlf;
+int BirthDay;
+int BirthMonth;
+int BirthYear;
